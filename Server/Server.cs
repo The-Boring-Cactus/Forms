0caf1db1a1cd72321103601b1c938f3ac82b3032
Ricardo Rodriguez <ricardo.rodriguez@c4t.dev>
2023-08-06 17:30:30 -0500

    New update

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,11 +1,6 @@
 ﻿using NetCoreServer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 namespace bpaforms.Server
 {
     public class Server : WsServer
